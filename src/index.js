ca0d904aa7b1f0beb9e29879eb81be416dd91268
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-11-23 19:23:39 +0100

    Import display functions and execute them on click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
 import './style.css'
+import displayHome from './home.js'
+import displayMenu from './menu.js'
+import displayAbout from './about-us.js'
 
 
-console.log('ejfhvedvjt')
+displayHome()
+
+const home = document.querySelector('.navhome')
+const menu = document.querySelector('.navmenu')
+const about = document.querySelector('.navabout')
+const container = document.querySelector('div#content')
+var homeBtn = document.querySelector('button')
+
+home.addEventListener('click', () => {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    displayHome()
+    
+    homeBtn = document.querySelector('button')
+    homeBtn.addEventListener('click', () => {
+        while(container.firstChild){
+            container.removeChild(container.firstChild);
+        }
+        displayMenu()
+    })
+})
+
+menu.addEventListener('click', () => {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    displayMenu()
+})
+
+about.addEventListener('click', () => {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    displayAbout()
+})
+
+homeBtn.addEventListener('click', () => {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    displayMenu()
+})
